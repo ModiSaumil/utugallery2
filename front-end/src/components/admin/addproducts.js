438468d591e555cb2f9859da96988f4af32097dc
ModiSaumil <19bmiit001@gmail.com>
2022-09-25 16:39:24 +0530

    SDCV

--- front-end/src/components/admin/addproducts.js
+++ front-end/src/components/admin/addproducts.js
@@ -6,27 +6,26 @@ const AddProduct = () => {
     const navigate = useNavigate('');
     const [imgname, setName] = React.useState('');
     const [tag, setTag] = React.useState('');
-    const [path, setPath] = React.useState('');
+    
 
     const addProduct = async () => {
-        
+        console.warn(imgname, tag);
         const userid = JSON.parse(localStorage.getItem("user"))._id;
         let result =await fetch("http://localhost:5000/addproduct", {
                 method:'post',
-                body:JSON.stringify({imgname, tag,userid}),
+                body:JSON.stringify({imgname, tag, userid}),
                 headers:{
                     'Content-Type':'application/json'
                    },
         });
        result = await result.json()
-       console.warn(imgname, tag, path);
        alert("image uploaded..")
        navigate('/')
        console.warn(result);
     }
 
-    const handleInputChange = (e)=>{
-        setPath(e.target.value)
+    const handleInputChange = ()=>{
+        
     }
 
     return (
@@ -36,7 +35,7 @@ const AddProduct = () => {
                 value={imgname} onChange={(e) => setName(e.target.value)} />
             <input className="inputbox" type="text" placeholder="enter tag"
                 value={tag} onChange={(e) => setTag(e.target.value)} />
-          <form><input  type="file" value={path}  className="inputbox" name="upload_file" onChange={handleInputChange} ></input>
+          <form><input  type="file" className="inputbox" name="upload_file" onChange={handleInputChange} ></input>
          </form>   <button onClick={addProduct} type="button" className="btnsn">Add photo</button>
 
         </div>
