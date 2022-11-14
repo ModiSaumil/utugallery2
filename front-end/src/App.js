1500ae30c74b165702fa66ce14c21b95eededd6d
ModiSaumil <19bmiit001@gmail.com>
2022-11-14 22:29:12 +0530

    n

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -28,7 +28,7 @@ function App() {
         <Route path="/SignUp" element={<SignUp />}></Route>
         <Route path="/Login" element={<Login />}></Route>
       </Routes>
-
+      
       </BrowserRouter>
       <Footer />
     </div>
